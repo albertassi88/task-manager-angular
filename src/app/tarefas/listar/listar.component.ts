af43e72ff6e2a525ed570ab10f6afaac0718f51e
albertassi88 <albertassi88@gmail.com>
2022-01-02 16:44:44 -0300

    cricao da implementacao do botao remover tarefas

--- src/app/tarefas/listar/listar.component.ts
+++ src/app/tarefas/listar/listar.component.ts
@@ -21,4 +21,19 @@ export class ListarComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if(confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) { //confirm é um método do próprio navegador que exibe um alert
+      this.tarefaService.remover(Number(tarefa.id));
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa) :void {
+    if(confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) { //confirm é um método do próprio navegador que exibe um alert
+      this.tarefaService.alterarStatus(Number(tarefa.id));
+      this.tarefas = this.listarTodos();
+    }
+  }
+
 }
